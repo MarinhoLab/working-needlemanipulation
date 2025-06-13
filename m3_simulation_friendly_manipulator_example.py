8556693a2e7706d547bef5e24cb7b35955767620
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-06-13 07:53:46 +0100

    Basic example working

--- m3_simulation_friendly_manipulator_example.py
+++
@@ -1,7 +0,0 @@
-from marinholab.working.needlemanipulation import M3_SerialManipulatorSimulatorFriendly
-
-def main():
-    robot = M3_SerialManipulatorSimulatorFriendly()
-
-if __name__ == "__main__":
-    main()
--- marinholab/working/needlemanipulation/__init__.py
+++ marinholab/working/needlemanipulation/__init__.py
@@ -2,4 +2,4 @@ from dqrobotics import *
 from dqrobotics.robot_modeling import DQ_SerialManipulator
 from marinholab.working.needlemanipulation._core import *
 
-from _impl import needle_jacobian, needle_entry_error
+from marinholab.working.needlemanipulation._impl import needle_jacobian, needle_entry_error
