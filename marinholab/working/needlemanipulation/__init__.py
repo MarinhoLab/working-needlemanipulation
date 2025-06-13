f5f480bda5f15cc9c3ab2fe86b60fd2b94c9b696
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-06-13 07:11:41 +0100

    Adding Jacobian impl

--- marinholab/working/needlemanipulation/__init__.py
+++ marinholab/working/needlemanipulation/__init__.py
@@ -1,3 +1,5 @@
 from dqrobotics import *
 from dqrobotics.robot_modeling import DQ_SerialManipulator
 from marinholab.working.needlemanipulation._core import *
+
+from _impl import needle_jacobian, needle_entry_error
