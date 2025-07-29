88f56c83ce56aa332d15320e86e9c7d73361bce4
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-29 02:49:50 +0100

    Adding needle rotation constraint.

--- marinholab/working/needlemanipulation/_impl.py
+++ marinholab/working/needlemanipulation/_impl.py
@@ -10,6 +10,31 @@ from dqrobotics.utils import DQ_Geometry
 from dqrobotics.robot_modeling import DQ_Kinematics
 from termcolor import cprint
 
+def rotation_axis_jacobian(primitive: DQ,
+                           r: DQ,
+                           Jr: np.ndarray):
+    """
+    https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8742769
+    See Eq (26)
+    Returns: The suitable Jacobian matrix.
+    """
+    return haminus4(k_ * conj(r)) @ Jr \
+           + hamiplus4(r * k_) @ C4() @ Jr
+
+def _l_normal_dot_product_Jacobian(normals: list[DQ],
+                                   primitive: DQ,
+                                   r: DQ,
+                                   Jr: np.ndarray) -> np.ndarray:
+    W = None
+
+    for normal in normals:
+        J = vec4(normal).T @ rotation_axis_jacobian(primitive, r, Jr)
+        if W is None:
+            W = J
+        else:
+            np.vstack((W, J))
+
+    return W
 
 def needle_jacobian(Jx_needle, x_needle: DQ, ps_vessel: list[DQ]):
     """
@@ -19,8 +44,12 @@ def needle_jacobian(Jx_needle, x_needle: DQ, ps_vessel: list[DQ]):
     p_vessel: The position of the entry point in the vessel
     """
     p_needle = translation(x_needle)
+    # Radius constraint
     Jt_needle = DQ_Kinematics.translation_jacobian(Jx_needle, x_needle)
+    # Plane constraint
     Jpi_needle = DQ_Kinematics.plane_jacobian(Jx_needle, x_needle, k_)
+    # Two-point line rotation constraint
+
 
     W_needle = None
 
